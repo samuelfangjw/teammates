5178fb54c1309054633b2683a65a10e2c4db1513
Samuel Fang <samuelfangjw@gmail.com>
2021-06-09 01:04:23 +0800

    Add downloading of proof of concept from student home page

--- src/web/app/pages-student/student-home-page/student-home-page.component.ts
+++ src/web/app/pages-student/student-home-page/student-home-page.component.ts
@@ -1,22 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { finalize } from 'rxjs/operators';
-import { CourseService } from '../../../services/course.service';
-import { FeedbackSessionsService } from '../../../services/feedback-sessions.service';
-import { StatusMessageService } from '../../../services/status-message.service';
-import { TableComparatorService } from '../../../services/table-comparator.service';
-import { TimezoneService } from '../../../services/timezone.service';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {finalize} from 'rxjs/operators';
+import {saveAs} from 'file-saver';
+import {CourseService} from '../../../services/course.service';
+import {FeedbackSessionsService} from '../../../services/feedback-sessions.service';
+import {StatusMessageService} from '../../../services/status-message.service';
+import {TableComparatorService} from '../../../services/table-comparator.service';
+import {TimezoneService} from '../../../services/timezone.service';
 import {
   Course,
-  Courses,
+  Courses, FeedbackQuestion,
+  FeedbackQuestions,
   FeedbackSession,
   FeedbackSessionPublishStatus,
   FeedbackSessions,
   FeedbackSessionSubmissionStatus,
-  HasResponses,
+  HasResponses, Student,
 } from '../../../types/api-output';
-import { SortBy, SortOrder } from '../../../types/sort-properties';
-import { ErrorMessageOutput } from '../../error-message-output';
+import {SortBy, SortOrder} from '../../../types/sort-properties';
+import {ErrorMessageOutput} from '../../error-message-output';
+import {FeedbackQuestionsService} from "../../../services/feedback-questions.service";
+import {Intent} from "../../../types/api-request";
+import {FeedbackResponsesService} from "../../../services/feedback-responses.service";
+import {FeedbackResponseDetailsFactory} from "../../../types/response-details-impl/feedback-response-details-factory";
+import {StudentService} from "../../../services/student.service";
 
 interface StudentCourse {
   course: Course;
@@ -65,11 +72,14 @@ export class StudentHomePageComponent implements OnInit {
   sortBy: SortBy = SortBy.NONE;
 
   constructor(private route: ActivatedRoute,
-    private courseService: CourseService,
-    private statusMessageService: StatusMessageService,
-    private feedbackSessionsService: FeedbackSessionsService,
-    private timezoneService: TimezoneService,
-    private tableComparatorService: TableComparatorService) {
+              private courseService: CourseService,
+              private statusMessageService: StatusMessageService,
+              private studentService: StudentService,
+              private feedbackSessionsService: FeedbackSessionsService,
+              private feedbackQuestionsService: FeedbackQuestionsService,
+              private feedbackResponsesService: FeedbackResponsesService,
+              private timezoneService: TimezoneService,
+              private tableComparatorService: TableComparatorService) {
     this.timezoneService.getTzVersion();
   }
 
@@ -109,7 +119,7 @@ export class StudentHomePageComponent implements OnInit {
                 const isAllSessionsPresent: boolean =
                   sortedFss.filter((fs: FeedbackSession) =>
                     sessionsReturned.has(fs.feedbackSessionName)).length
-                    === sortedFss.length;
+                  === sortedFss.length;
 
                 if (!isAllSessionsPresent) {
                   this.statusMessageService.showErrorToast(this.allStudentFeedbackSessionsNotReturned);
@@ -125,14 +135,14 @@ export class StudentHomePageComponent implements OnInit {
 
                   const isSubmitted: boolean = hasRes.hasResponsesBySession[fs.feedbackSessionName];
                   studentSessions.push(Object.assign({},
-                    { isOpened, isWaitingToOpen, isPublished, isSubmitted, session: fs }));
+                    {isOpened, isWaitingToOpen, isPublished, isSubmitted, session: fs}));
                 }
               }, (error: ErrorMessageOutput) => {
                 this.hasCoursesLoadingFailed = true;
                 this.statusMessageService.showErrorToast(error.error.message);
               });
 
-            this.courses.push(Object.assign({}, { course, feedbackSessions: studentSessions }));
+            this.courses.push(Object.assign({}, {course, feedbackSessions: studentSessions}));
             this.courses.sort((a: StudentCourse, b: StudentCourse) =>
               (a.course.courseId > b.course.courseId) ? 1 : -1);
           }, (error: ErrorMessageOutput) => {
@@ -210,5 +220,95 @@ export class StudentHomePageComponent implements OnInit {
       }
       return this.tableComparatorService.compare(by, SortOrder.ASC, strA, strB);
     });
+  }
+
+  /**
+   * Triggers the download of a student's response as a text file.
+   */
+  downloadResponse(rowIndex: number): void {
+    console.log(rowIndex);
+  }
+
+  /**
+   * Triggers the download of a student's submission as a text file.
+   */
+  downloadSubmission(rowIndex: number, studentCourse: StudentCourse): void {
+    const studentSession: StudentSession = studentCourse.feedbackSessions[rowIndex];
+    const courseId = studentSession.session.courseId;
+
+    this.getStudent(courseId, (student: Student) => {
+      this.processSubmission(student, studentSession);
+    })
+  }
+
+  /**
+   * Fetch the student, following which calls {@param onComplete} with the student.
+   * @param courseId The courseId the student is in.
+   * @param onComplete The function to call once student is fetched.
+   */
+  getStudent(courseId: string, onComplete: (student: Student) => void) {
+    this.studentService.getStudent(courseId).subscribe((student: Student) => {
+      onComplete(student);
+    })
+  }
+
+  processSubmission(student: Student, studentSession: StudentSession) {
+    const feedbackSession = studentSession.session;
+    const time: number = new Date().getTime();
+    const formattedTime: string = this.timezoneService.formatToString(
+      time, feedbackSession.timeZone, 'YYYYMMDDHHmmSSZZ');
+
+    const fileContent: string[] = [
+      'TEAMMATES Proof of Submission',
+      `Updated as at ${time}::${formattedTime}`,
+      '==============================',
+      `Submitted by: ${student.name} [${student.email}]`,
+      `Course: ${feedbackSession.courseId}`,
+      `Session: ${feedbackSession.feedbackSessionName}`,
+      '',
+      '',
+      '',
+    ];
+
+    this.feedbackQuestionsService.getFeedbackQuestions({
+      courseId: feedbackSession.courseId,
+      feedbackSessionName: feedbackSession.feedbackSessionName,
+      intent: Intent.STUDENT_SUBMISSION
+    }).subscribe(async (response: FeedbackQuestions) => {
+      this.processQuestions(response.questions, 0, fileContent, time);
+    })
+  }
+
+  processQuestions(questions: FeedbackQuestion[], idx: number, fileContent: string[], time: number) {
+    const question = questions[idx];
+
+    this.feedbackResponsesService.getFeedbackResponse({
+      questionId: question.feedbackQuestionId,
+      intent: Intent.STUDENT_SUBMISSION,
+      key: '',
+      moderatedPerson: '',
+    }).subscribe({
+      next: answers => {
+        fileContent.push(`${question.questionNumber}: ${question.questionBrief}`);
+
+        answers.responses.forEach(answer => {
+          fileContent.push(`> ${answer.recipientIdentifier}`);
+          fileContent.push(FeedbackResponseDetailsFactory
+            .fromApiOutput(answer.responseDetails)
+            .getResponseCsvAnswers(question.questionDetails)
+            .join(','));
+          fileContent.push('');
+          fileContent.push('');
+        })
+      },
+      complete: () => {
+        if (idx >= questions.length - 1) {
+          const blob: Blob = new Blob([fileContent.join('\r\n')], {type: 'text/plain'});
+          saveAs(blob, `TEAMMATES Proof of Submission - ${time}`);
+        } else {
+          this.processQuestions(questions, idx + 1, fileContent, time);
+        }
+      }
+    })
   }
 }
