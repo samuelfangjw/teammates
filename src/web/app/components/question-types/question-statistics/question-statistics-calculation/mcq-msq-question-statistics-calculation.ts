6ff1629025b509c6f39e57a6c6e45e5ffdeca3be
Samuel Fang <60355570+samuelfangjw@users.noreply.github.com>
2026-05-13 06:49:40 +0800

    Add question statistics module

--- src/web/app/components/question-types/question-statistics/question-statistics-calculation/mcq-msq-question-statistics-calculation.ts
+++ src/web/app/components/question-types/question-statistics/question-statistics-calculation/mcq-msq-question-statistics-calculation.ts
@@ -2,6 +2,7 @@
  * Interface defines common fields for MCQ MSQ stats calculation.
  */
 export interface McqMsqQuestionStatisticsCalculation {
+  // TODO: remove this interface before merging
   answerFrequency: Record<string, number>;
   percentagePerOption: Record<string, number>;
   weightPerOption: Record<string, number>;
