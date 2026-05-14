7550f15be6c348fd951fb0f55d6df5471d1eff09
Yong Jun Xi <89063663+TobyCyan@users.noreply.github.com>
2026-05-15 01:44:56 +0800

    [#13918] Add accountId to UserInfoCookie and UserInfo (#13919)

--- src/web/types/api-output.ts
+++ src/web/types/api-output.ts
@@ -620,6 +620,7 @@ export interface UsageStatisticsRange extends ApiOutput {
 
 export interface UserInfo {
   id: string;
+  accountId: string;
   isAdmin: boolean;
   isInstructor: boolean;
   isStudent: boolean;
